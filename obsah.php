d3017b400af49879b5fb3ba523689696a6de998c
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-19 22:27:48 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,4 +1,9 @@
 <?php
+copy("https://raw.githubusercontent.com/bedjan/web/main/Parsedown.php", "Parsedown.php");
+?>
+
+
+<?php
 copy("https://raw.githubusercontent.com/bedjan/web/main/web.md", "web.md");
 ?>
 
