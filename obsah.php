96a848a319dc10652a5e6ed89c21cf2afb73735a
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-19 22:33:02 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,5 +1,9 @@
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/Parsedown.php", "Parsedown.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "mm.php");
+?>
+
+<?php
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "linux.php");
 ?>
 
 
@@ -11,10 +15,4 @@ copy("https://raw.githubusercontent.com/bedjan/web/main/web.md", "web.md");
 copy("https://raw.githubusercontent.com/bedjan/web/main/mm.md", "mm.md");
 ?>
 
-<?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "mm.php");
-?>
 
-<?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "linux.php");
-?>
