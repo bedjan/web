d2c03710b75b039d7f518191f1fe74b9f1269635
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-19 22:34:17 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+copy("https://raw.githubusercontent.com/bedjan/web/main/Parsedown.php", "Parsedown.php");
+?>
+
+<?php
 include('Parsedown.php');
 $html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/web.md');
 $Parsedown = new Parsedown();
