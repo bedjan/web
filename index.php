847fb775e0ca938a6a9263873f75a7c192ad6665
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-19 22:16:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 include('Parsedown.php');
-$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/index.md');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/web.md');
 $Parsedown = new Parsedown();
 echo $Parsedown->text($html);
+?>
+
+<?php
+copy("https://raw.githubusercontent.com/bedjan/web/main/obsah.php", "obsah.php");
+?>
+
+<?php
+include('obsah.php');
 ?>
