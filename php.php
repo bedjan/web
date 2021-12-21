03cef0746791d85729c2debd4166753f7ea041a5
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 13:11:31 +0100

    Create php.php

---
+++ php.php
@@ -0,0 +1,11 @@
+
+<?php
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.md');
+$html1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/php.md');
+$html2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+echo $Parsedown->text($html1);
+echo $Parsedown->text($html2);
+?>
