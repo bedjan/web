c03b5c2c4a934dfa4cafbf3df53cad0e60cee4b8
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 11:09:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/Parsedown.php", "Parsedown.php");
-copy("https://raw.githubusercontent.com/bedjan/web/main/obsah.php", "obsah.php");
+include('https://raw.githubusercontent.com/bedjan/web/main/obsah.php');
 ?>
-
-<?php
-include('Parsedown.php');
-$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.md');
-$html1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/web.md');
-$html2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.md');
-$Parsedown = new Parsedown();
-echo $Parsedown->text($html);
-echo $Parsedown->text($html1);
-echo $Parsedown->text($html2);
-?>
-
-
-
-<?php
-include('obsah.php');
-?>
-
-
