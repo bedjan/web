51a59c9b28f5167088868b39912be9be855423e1
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 10:34:55 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,9 +1,9 @@
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "mm.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "./page/mm.php");
 ?>
 
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "linux.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "./page/linux.php");
 ?>
 
 
