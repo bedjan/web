8cae9e35d839d5dff1c34066388315816541dab8
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 13:04:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-include('https://raw.githubusercontent.com/bedjan/web/main/obsah.php');
+$filename = 'obsah.php';
+
+if (file_exists($filename)) {
+copy("https://raw.githubusercontent.com/bedjan/web/main/obsah.php", "obsah.php");
+include('obsah.php');
+} else {
+copy("https://raw.githubusercontent.com/bedjan/web/main/obsah.php", "obsah.php");
+include('obsah.php');
+}
 ?>
