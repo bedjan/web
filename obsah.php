4d142ecc3d6a2819589e6de28617c94498c4a1f2
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 11:10:00 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,10 +1,28 @@
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "./page/mm.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "mm.php");
 ?>
 
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "./page/linux.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "linux.php");
 ?>
+
+
+<?php
+copy("https://raw.githubusercontent.com/bedjan/web/main/Parsedown.php", "Parsedown.php");
+
+?>
+
+<?php
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.md');
+$html1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/web.md');
+$html2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+echo $Parsedown->text($html1);
+echo $Parsedown->text($html2);
+?>
+
 
 
 
