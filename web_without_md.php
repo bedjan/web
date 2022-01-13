c172537f5cb9fa60af4ca9c15adce91c7ae37106
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-13 19:05:42 +0100

    Update web_without_md.php

--- web_without_md.php
+++ web_without_md.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, viewport-fit=cover">
+<meta name="viewport" content="width=device-width">
 
 <base target="_blank">
 <style>
@@ -48,52 +48,6 @@ form {
   margin-left: 2px;
 } 
 
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video 
-{
-//    background-color: black;
-//    color: white; 
-  margin: 3px;
-  padding: 3px;
-  border: 3px;
-//  font-size: 100%;
-//  font: inherit;
-//    vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, menu, nav, section {
-  display: block;
-}
-body {
-  line-height: 1;
-}
-ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
 
 
 // html {width: 100%;}
