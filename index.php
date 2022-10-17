7179cde3fdf7a9fcb2e4cdfa02724f7c322fbf82
bedjan <29821749+bedjan@users.noreply.github.com>
2022-10-17 12:35:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 $filename = 'obsah.php';
 
 if (file_exists($filename)) {
-copy("https://raw.githubusercontent.com/bedjan/web/main/obsah.php", "obsah.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/web_without_md.php", "obsah.php");
 include('obsah.php');
 } else {
-copy("https://raw.githubusercontent.com/bedjan/web/main/obsah.php", "obsah.php");
+copy("https://raw.githubusercontent.com/bedjan/web/main/web_without_md.php", "obsah.php");
 include('obsah.php');
 }
 ?>
