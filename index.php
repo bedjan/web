2376643fabff690625662df363ca3581895595db
bedjan <29821749+bedjan@users.noreply.github.com>
2024-06-27 12:42:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-$filename = 'obsah.php';
+$filename = 'index.html';
 
 if (file_exists($filename)) {
-copy("https://raw.githubusercontent.com/bedjan/web/main/web_without_md.php", "obsah.php");
-include('obsah.php');
+copy("https://raw.githubusercontent.com/bedjan/itpoznamky/main/index_itpoznamky.html", "index.html");
+include('index.html');
 } else {
-copy("https://raw.githubusercontent.com/bedjan/web/main/web_without_md.php", "obsah.php");
-include('obsah.php');
+copy("https://raw.githubusercontent.com/bedjan/itpoznamky/main/index_itpoznamky.html", "index.html");
+include('index.html');
 }
 ?>
